9d27c09071614e8e36f43c7829146a15050d6db1
Davidasx <81460770+Davidasx@users.noreply.github.com>
2021-07-04 20:10:19 +0800

    Create ubuntugui.cpp

---
+++ ubuntugui.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+#include<windows.h>
+using namespace std;
+int main(){
+	ios::sync_with_stdio(0);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	cout<<"Launch GUI or just CLI?[G/c]"<<flush;
+	string s;
+	getline(cin,s);
+	for(int i=0;i<s.size();i++) if(s[i]>='a'&&s[i]<='z')
+		s[i]-='a',s[i]+='A';
+	if(s[0]!='C'){
+		cout<<"Use GUI..."<<endl;
+		system("config.xlaunch");
+		Sleep(2000);
+		system("ubuntu run starter.sh");
+	}
+	else{
+		cout<<"Use CLI..."<<endl;
+		system("wt new-tab -p Ubuntu");//If you use Windows Terminal
+    //or system("start Ubuntu") if you don't
+	}
+	return 0;
+}
